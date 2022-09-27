9f8c7e05142e62b720c4eb359a0231c11855194a
Jean Pirul <jean.pirul@trebol-it.com>
2022-09-27 15:27:04 -0300

    se elimina register

--- src/routes/appRegister.ts
+++ src/routes/appRegister.ts
@@ -1,13 +1,7 @@
 import express from "express";
 import { login } from "../controllers/login.controller";
-import { register } from "../controllers/register.controller";
 
 const router = express.Router();
-
-router.post("/register", async (req, res) => {
-  //este endpoint permite crear un nuevo cliente
-  await register(req, res);
-});
 
 router.post("/login", async (req, res) => {
   //este endpoint permite crear un nuevo cliente
